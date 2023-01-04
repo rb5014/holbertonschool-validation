3ad0740cb4479be4ff4e305aa7f484e95917a61b
Romain Bayle <5014@holbertonstudents.com>
2023-01-04 16:48:34 +0100

    task 4 test

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo 'apt-get update && apt-get install hugo make && apt-get install curl -y && mkdir /hugo &&
 curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb &&
 apt-get install ./install_hugo.deb &&
