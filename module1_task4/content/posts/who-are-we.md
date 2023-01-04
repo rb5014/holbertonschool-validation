c268b34b222074eec71c484b5707d9e4090ebee7
Romain Bayle <5014@holbertonstudents.com>
2023-01-04 16:34:09 +0100

    task4

---
+++ module1_task4/content/posts/who-are-we.md
@@ -0,0 +1,6 @@
+---
+title: "Who are we"
+date: 2023-01-03T13:14:19+01:00
+draft: true
+---
+
---
+++ module1_task4/setup.sh
@@ -0,0 +1,4 @@
+echo 'apt-get update && apt-get install curl -y && mkdir /hugo &&
+curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb &&
+apt-get install ./install_hugo.deb make &&
+make build' | docker run --rm  --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash 
