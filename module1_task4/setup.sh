e8ffaab859a6651de618195df5f4ae89016306f9
Romain Bayle <5014@holbertonstudents.com>
2023-01-04 16:44:03 +0100

    task 4 fixed

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,4 +1,4 @@
-echo 'apt-get update && apt-get install curl -y && mkdir /hugo &&
+echo 'apt-get update && apt-get install curl hugo make -y && mkdir /hugo &&
 curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb &&
-apt-get install ./install_hugo.deb make &&
+apt-get install ./install_hugo.deb &&
 make build' | docker run --rm  --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash 
