eb3f93d73cce19305536bf52b6f62759728f2534
Romain Bayle <5014@holbertonstudents.com>
2023-01-05 13:29:15 +0100

    workflows dir created

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,6 +1,3 @@
-# Spawn a sandboxed environment with ubuntu 18.04
-docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
-
 # Install 'hugo' and 'make'
 apt-get update && apt-get install -y hugo make
 
