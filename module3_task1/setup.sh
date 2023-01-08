bb66b4be6ec697279fcf6cc3c94814874c9e44c0
Romain Bayle <5014@holbertonstudents.com>
2023-01-08 18:58:54 +0100

    module3 task 1

---
+++ module3_task1/setup.sh
@@ -0,0 +1,7 @@
+# Update
+apt-get update 
+# Download latest release of 'hugo'
+curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+# Install the latest version of 'hugo'
+apt-get install ./install_hugo.deb
+
