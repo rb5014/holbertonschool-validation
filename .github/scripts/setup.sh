931e96d3edb9c4ae4867da31d08449bdaea1a5a4
Romain Bayle <5014@holbertonstudents.com>
2023-01-08 23:30:56 +0100

    setup.sh removed curl install and update

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -1,9 +1,3 @@
-# Update
-sudo apt-get update
-
-# Install 'curl'
-sudo apt-get install curl -y
-
 # Download latest release of 'hugo'
 sudo curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
 
