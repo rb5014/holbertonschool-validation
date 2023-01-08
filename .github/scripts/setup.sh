ae078585a456ba7396ad206c9bb4d6ef0ba23370
Romain Bayle <5014@holbertonstudents.com>
2023-01-08 23:33:55 +0100

    setup reverted to old state

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -1,6 +1,14 @@
+# Update and install 'hugo' and 'make'
+apt-get update && apt-get install hugo make
+
+# Install 'curl'
+apt-get install curl -y
+
 # Download latest release of 'hugo'
-sudo curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
 
 # Install the latest version of 'hugo'
-sudo apt-get install ./install_hugo.deb
+apt-get install ./install_hugo.deb
 
+# Build the website
+make build
