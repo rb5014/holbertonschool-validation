c8e0bda3d3e448a2f8eae81162aa8895002f0e79
Romain Bayle <5014@holbertonstudents.com>
2023-01-08 23:26:58 +0100

    fixed setup added install curl

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -1,7 +1,12 @@
 # Update
-sudo apt-get update 
+sudo apt-get update
+
+# Install 'curl'
+sudo apt-get install curl -y
+
 # Download latest release of 'hugo'
-curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+sudo curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+
 # Install the latest version of 'hugo'
 sudo apt-get install ./install_hugo.deb
 
