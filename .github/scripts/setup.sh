788bd6e44577e83d6efcc350387d87a1d75ce9d2
Romain Bayle <5014@holbertonstudents.com>
2023-01-08 23:41:15 +0100

    setup changed

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -1,12 +1,3 @@
-# Update and install 'hugo' and 'make'
-sudo apt-get update && sudo apt-get install hugo
-
-# Install 'curl'
-sudo apt-get install curl -y
-
-# Download latest release of 'hugo'
-sudo curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-
-# Install the latest version of 'hugo'
-sudo apt-get install ./install_hugo.deb
-
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+sudo apt install ./hugo.deb
+sudo rm hugo.deb
