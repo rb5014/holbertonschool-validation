cb7c7c6a1ab76b31f18b147f2af27167eb6c828c
Romain Bayle <5014@holbertonstudents.com>
2023-01-09 12:57:18 +0100

    without binbash

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-sudo apt install ./hugo.deb
-sudo rm hugo.deb
+docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
+sudo apt-get update && sudo apt-get install -y hugo make
+sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o last.deb
+sudo apt install ./last.deb
+sudo rm last.deb
+make build
