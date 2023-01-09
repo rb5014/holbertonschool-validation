a74c9b72594b06f7db50687332eeede86e358eb1
Romain Bayle <5014@holbertonstudents.com>
2023-01-09 13:12:32 +0100

    put install make again in setup.sh

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get update && sudo apt-get install -y hugo
+sudo apt-get update && sudo apt-get install -y hugo make
 sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o last.deb
 sudo apt install ./last.deb
 sudo rm last.deb
