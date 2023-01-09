dcd78989e98fc69ec054e0e377e24cb69afccbd6
Romain Bayle <5014@holbertonstudents.com>
2023-01-09 12:30:53 +0100

    test

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,3 +1,7 @@
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-sudo apt install ./hugo.deb
-sudo rm hugo.deb
+#!/bin/bash
+docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
+sudo apt-get update && sudo apt-get install -y hugo make
+sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o last.deb
+sudo apt install ./last.deb
+sudo rm last.deb
+make build
