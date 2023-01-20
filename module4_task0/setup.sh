90fe92a30b924490cff6228872c806b14ff93c8d
Romain Bayle <5014@holbertonstudents.com>
2023-01-20 16:57:18 +0100

    install zip because of make package

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,5 +1,5 @@
 # Update and install 'hugo' and 'make'
-apt-get update && apt-get install hugo make
+apt-get update && apt-get install -y hugo make
 
 # Install 'curl'
 apt-get install curl -y
@@ -8,7 +8,7 @@ apt-get install curl -y
 curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
 
 # Install the latest version of 'hugo'
-apt-get install ./install_hugo.deb
+apt-get install -y ./install_hugo.deb
 rm ./install_hugo.deb
 
 # Initialize go project
