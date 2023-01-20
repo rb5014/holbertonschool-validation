2c16a7f7581e9bb2b8c2ee42feb9e11a7ad228ba
Romain Bayle <5014@holbertonstudents.com>
2023-01-20 17:00:13 +0100

    install zip in setup instead of workflow

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -17,5 +17,8 @@ rm ./install_hugo.deb
 # Install golangci-lint
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.50.1
 
+# Install zip
+apt install zip -y
+
 # Build website
 make build
